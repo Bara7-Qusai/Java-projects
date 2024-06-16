a2b55dd26ffcaa65e75eca07c2ba5bd3b036c6ee
Bara7-Qusai <100060234+Bara7-Qusai@users.noreply.github.com>
2024-06-16 02:53:13 +0200

    Create Book.java

---
+++ Library Management System/Book.java
@@ -0,0 +1,48 @@
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private String category;
+    private boolean isBorrowed;
+
+    public Book(String id, String title, String author, String category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.isBorrowed = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrowBook() {
+        this.isBorrowed = true;
+    }
+
+    public void returnBook() {
+        this.isBorrowed = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Book ID: " + id + ", Title: " + title + ", Author: " + author + ", Category: " + category;
+    }
+}
